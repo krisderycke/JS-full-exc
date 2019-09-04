33aa89a409996f5a8cc5569e57073f346a6e2386
Kris de Rycke <derycke.kris@gmail.com>
2019-09-04 10:14:35 +0200

    complete array excercises

--- 3. Js-complete-course/05-arrays/11-dedupe-array/script.js
+++ 3. Js-complete-course/05-arrays/11-dedupe-array/script.js
@@ -10,24 +10,27 @@
 // You will have time to focus on it later.
 
 (function() {
+  var fruits = [
+    "cerise",
+    "durian",
+    "pomme",
+    "poire",
+    "fraise",
+    "tomate",
+    "orange",
+    "mandarine",
+    "fraise",
+    "durian",
+    "pêche",
+    "cerise",
+    "raisin",
+    "cerise"
+  ];
 
-    var fruits = [
-        "cerise",
-        "durian",
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "fraise",
-        "durian",
-        "pêche",
-        "cerise",
-        "raisin",
-        "cerise",
-    ];
-
-    // your code here
-
+  //explanation on duplicate items and such
+  // https://medium.com/dailyjs/how-to-remove-array-duplicates-in-es6-5daa8789641c
+  document.getElementById("run").addEventListener("click", function() {
+    var noDoubles = Array.from(new Set(fruits));
+    console.log(noDoubles);
+  });
 })();
